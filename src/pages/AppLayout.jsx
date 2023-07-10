6d733b8779efcf7b424dd8fda868f2b1c269fc47
Grégory Marini <greg.marini@hotmail.fr>
2023-07-10 15:00:00 +0200

    first commit

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from '../components/AppNav';
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>This wher my app is</p>
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Homepage() {
+  return (
+    <div>
+      <h1>WorldWise</h1>
+      <Link to='/app'>Go to the app</Link>
+    </div>
+  );
+}
+
+export default Homepage;
